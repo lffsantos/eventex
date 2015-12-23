8d7efc03ce9dc39dfd2be6ecf3910fae049dbf78
lucas <lffsantos@gmail.com>
2015-12-23 01:43:11 -0300

    inscricao screnn

--- eventex/urls.py
+++ eventex/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 from eventex.core.views import home
+from eventex.subscriptions.views import subscribe
 
 urlpatterns = [
     url(r'^$', home),
+    url(r'^inscricao/$', subscribe),
     url(r'^admin/', include(admin.site.urls)),
 ]

37d20b8423156c7e42667526372661f5899bd39d
lucas <lffsantos@gmail.com>
2015-12-25 02:59:40 -0300

    remmove method not used

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 from django.contrib import messages
-from django.core import mail
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.template.loader import render_to_string

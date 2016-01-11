9ce6f077a5d25f7bdad38c717c65cc64bb52b2db
lucas <lffsantos@gmail.com>
2016-01-11 15:56:41 -0300

    fix test admin mark_as_paid

--- eventex/subscriptions/tests/test_admin.py
+++ eventex/subscriptions/tests/test_admin.py
@@ -1,20 +1,40 @@
+from unittest.mock import Mock
 from django.test import TestCase
 from eventex.subscriptions.admin import SubscriptionModelAdmin, Subscription, admin
 
 
-class SubscriptionModelAdmin(TestCase):
-    def test_has_action(self):
-        # """ Action mark_as_paid should be installed"""
-        model_admin = SubscriptionModelAdmin(Subscription, admin.site)
-        self.assertIn('mark_as_paid', model_admin.actions)
-
-    def test_mark_all(self):
+class SubscriptionModelAdminTest(TestCase):
+    def setUp(self):
         Subscription.objects.create(name='Lucas farias', cpf='12345678901',
                                     email='lffsantos@gmail.com', phone='71-991625771')
 
+        self.model_admin = SubscriptionModelAdmin(Subscription, admin.site)
+    def test_has_action(self):
+        # """ Action mark_as_paid should be installed"""
+        self.assertIn('mark_as_paid', self.model_admin.actions)
+
+    def test_mark_all(self):
+
+        self.call_action()
+        self.assertEqual(1, Subscription.objects.filter(paid=True).count())
+
+
+    def test_message(self):
+        """It should a message to the user"""
+        mock = self.call_action()
+        mock.assert_called_with(None, '1 inscrição foi marcada como paga.')
+
+        SubscriptionModelAdmin.message_user = mock.old_message_user
+
+    def call_action(self):
         query_set = Subscription.objects.all()
 
-        model_admin = SubscriptionModelAdmin(Subscription, admin.site)
-        model_admin.mark_as_paid(None, query_set)
+        mock = Mock()
+        old_message_user = SubscriptionModelAdmin.message_user
+        SubscriptionModelAdmin.message_user = mock
 
-        self.assertEqual(1, Subscription.objects.filter(paid=True).count())
+        self.model_admin.mark_as_paid(None, query_set)
+
+        SubscriptionModelAdmin.message_user = old_message_user
+
+        return mock
